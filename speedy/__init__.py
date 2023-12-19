d350345cdd06685202242d8c666678629b8cb230
anh vo <anhvth.226@gmail.com>
2023-12-19 06:56:23 +0000

    update

--- speedy/__init__.py
+++ speedy/__init__.py
@@ -1,3 +1,4 @@
 from .utils import *
 from .file_io import *
 from .imports import *
+from .imports_ds import *
--- speedy/imports_ds.py
+++ speedy/imports_ds.py
@@ -1,8 +1,8 @@
 #File: speedy/imports_ds.py
+print('Imported pd, np, plt, torch, nn, F, glob')
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from glob import glob
